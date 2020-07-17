2dbfdc4793713e9e5fa0997fee136cb922a7447f
Тихонов Максим <tikhonov@Pregel.local>
2020-07-17 12:48:29 +0300

    add isNaN()

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -228,7 +228,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    return parseFloat(value) ? parseFloat(value) : def;
+    return isNaN(parseFloat(value)) ? def : parseFloat(value);
 }
 
 module.exports = {
