034749e201acbe65c4a86f89b2325d2857a5f750
Vpartyzan <peregrim.mt@gmail.com>
2020-07-27 22:58:07 +0300

    12 is done

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -299,7 +299,16 @@ function isCreditCardNumber(ccn) {
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
 function getDigitalRoot(num) {
-    throw new Error('Not implemented');
+    num = Array.from(num.toString());
+    let n = +num[0];
+
+    for (let i = 1; i < num.length; i++) {
+        n += +num[i];
+    }
+
+    num = Array.from(n.toString());
+    return (num.length === 1) ? n : +num[0] + +num[1];
+
 }
 
 
